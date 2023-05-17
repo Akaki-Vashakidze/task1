6d753d48f8737bbc114b46248c4da0015b3ba11b
akakivashakidze@Akakis-MBP.akaki-vashakidze <akakivashakidze@Akakis-MBP.akaki.vashakidze1@gmail.com>
2023-05-17 10:14:22 +0400

    delete works, but needs refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,17 +7,29 @@ import DataList from './components/list';
 function App() {
   
   const data = useStore((state) => state.data)
+  const saveServerData = useStore((state) => state.loadData)
+
   useEffect(()=> {
     getData()
   },[])
-  console.log(data)
+
+
+  axios.post('api/data',data)
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
   const onDataChange = () => {
-    console.log('data changed')
+  // console.log('data changed')
+  // console.log(data)
   }
-  const saveServerData = useStore((state) => state.loadData)
 
   const getData = () => {
-    axios.get('api')
+    axios.get('api/data')
     .then(res => {
       saveServerData(res.data.data)
     }).catch(err => {
--- server/server.js
+++ server/server.js
@@ -2,27 +2,42 @@ const express = require('express')
 const data = require('./data.json')
 const app = express()
 const fs = require('fs')
+const bodyParser = require('body-parser')
 
-fs.readFile('./data.json','utf-8',(err,jsonData) => {
-    if(err){
-        console.log(err)
-    } else {
-        try {
-             const data = JSON.parse(jsonData)
-        } catch {
-            console.log('error parsing data', err)
+let readDataFile;
+
+async function readData(res) {
+    fs.readFile('./data.json', 'utf-8', async (err, jsonData) => {
+        if (err) {
+            console.log(err)
+        } else {
+            try {
+                readDataFile = await JSON.parse(jsonData)
+                res.json({
+                    'data':readDataFile
+                })
+            } catch {
+                console.log('error parsing data', err)
+            }
         }
-    }
-})
+    })
+}
+
 
 
 // დაას დააფდეითება
-fs.writeFileSync('./data.json', JSON.stringify('რაღაც ახალი დათა'));
+// fs.writeFileSync('./data.json', JSON.stringify('რაღაც ახალი დათა'));
 
-app.get("/api", (req, res) => {
+app.get("/api/data", (req, res) => {
     res.json({
         "data": data
     })
+})
+
+app.post("/api/data", bodyParser.json(), (req, res) => {
+    const data = req.body
+    fs.writeFileSync('./data.json', JSON.stringify(data));
+    readData(res)
 })
 
 app.listen(3500, () => { console.log("server started on port 3500") })
